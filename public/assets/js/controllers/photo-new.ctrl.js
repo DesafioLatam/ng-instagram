204a871f5508907a3eff2dc927d3951b15549346
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-07-21 18:10:04 -0500

    responsive photos and redirecting to photo index after upload photo

--- public/assets/js/controllers/photo-new.ctrl.js
+++ public/assets/js/controllers/photo-new.ctrl.js
@@ -1,6 +1,6 @@
 angular.module('app')
   .controller('PhotoNewCtrl', 
-    ['$scope', '$http', '$window', 'PhotoApi', 'baseUrl', function($scope, $http, $window, PhotoApi, baseUrl){
+    ['$scope', '$http', '$window', '$location', 'PhotoApi', 'baseUrl', function($scope, $http, $window, $location, PhotoApi, baseUrl){
 
       $scope.base_url = baseUrl
       $scope.token = $window.sessionStorage.token
@@ -11,7 +11,7 @@ angular.module('app')
         PhotoApi.create($scope.photo, $scope.comment).then(function(http_object){
           console.log(http_object);
           $scope.photos = http_object.data
-
+          $location.path( "/photos" );
         }).catch(function(e){console.log(e)});
       }
     }]);
